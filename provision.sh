53a15d02afe5e6d326bbcdd346d8de04e319516e
Danail Branekov <danailster@gmail.com>
2024-04-03 13:10:32 +0000

    Bump Golang to 1.22.1
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- provision.sh
+++ provision.sh
@@ -184,7 +184,7 @@ install_golang() {
   echo ">>> Installing Golang"
   rm -rf /usr/local/go
 
-  GO_VERSION=1.21.5
+  GO_VERSION=1.22.1
   wget "https://go.dev/dl/go$GO_VERSION.linux-amd64.tar.gz"
   tar -C /usr/local -xzf "go$GO_VERSION.linux-amd64.tar.gz"
   rm "go$GO_VERSION.linux-amd64.tar.gz"
