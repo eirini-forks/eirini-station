e7f24ded8cd00a7abfc4276312fb2cb7dbc3f0b1
Georgi Sabev <georgethebeatle@gmail.com>
2025-02-24 17:42:36 +0200

    Write details about dev station in zsh profile
    This way we can avoid slow openstack calls most of the time, since the
    machine ips are stable as we rerely restart or recreate them

--- openstack/ssh.sh
+++ openstack/ssh.sh
@@ -2,18 +2,31 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-source "$SCRIPT_DIR/common.sh"
 
-if [[ "$STATION_STATUS" != "ACTIVE" ]]; then
-  echo "Station is not running. Run 'station start' to start it"
-  exit 1
+if [[ -z "${STATION_IP:-}" ]]; then
+  source "$SCRIPT_DIR/common.sh"
 fi
 
-ZSH_CUSTOM_DIR="/home/$VMUSER/.oh-my-zsh/custom"
+common_profile_path=".oh-my-zsh/custom/station.zsh"
+local_station_profile="$HOME/$common_profile_path"
+station_profile_contents=$(
+  cat <<-EOF
+export STATION_IP=$STATION_IP
+export VMUSER=$VMUSER
+EOF
+)
+
+mkdir -p "$(dirname "$local_station_profile")"
+echo "$station_profile_contents" >"$local_station_profile"
+
 for attempt in $(seq 10); do
-  if ! ssh \
-    -A "$VMUSER@$STATION_IP" \
-    "mkdir -p $ZSH_CUSTOM_DIR; echo export STATION_IP=$STATION_IP > $ZSH_CUSTOM_DIR/station_ip.zsh"; then
+  if
+    ! scp \
+      -A \
+      -o "UserKnownHostsFile=/dev/null" \
+      -o "StrictHostKeyChecking no" \
+      "$local_station_profile" "${VMUSER}@${STATION_IP}:/home/$VMUSER/$common_profile_path"
+  then
     sleep 1
     continue
   fi
@@ -21,6 +34,7 @@ for attempt in $(seq 10); do
   ssh \
     -A \
     -o "UserKnownHostsFile=/dev/null" \
+    -o "StrictHostKeyChecking no" \
     "$@" \
     "${VMUSER}@${STATION_IP}"
   exit 0
