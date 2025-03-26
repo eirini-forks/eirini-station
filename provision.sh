1a76a3c908a98169b6211f44a3eabda1a50d31f9
Georgi Sabev <georgethebeatle@gmail.com>
2025-03-26 14:01:45 +0000

    Bumpt nodejs
    Copilot requires nodejs 18 or newer

--- provision.sh
+++ provision.sh
@@ -197,7 +197,7 @@ install_golang() {
 install_nodejs() {
   echo ">>> Installing NodeJS"
   curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor --batch --yes -o /etc/apt/trusted.gpg.d/nodesource.gpg
-  NODE_MAJOR=16
+  NODE_MAJOR=22
   echo "deb [signed-by=/etc/apt/trusted.gpg.d/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
   chmod 644 /etc/apt/trusted.gpg.d/nodesource.gpg
   apt-get update
